d03ce531de10fa55d10540275b9c7ab2df12d7f3
정찬원 <jcw5522@naver.com>
2025-03-16 15:11:56 +0900

    NewsItem추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-
+import { useState } from "react";
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const respone = await axios.get('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=6d2a8dda301d4c37aab928d46dd6446a', );
+      setData(respone.data);
+    } catch(e) {
+      console.log(e);
+    }
+  };
 
   return (
     <div>
-      
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && <textarea rows = {7} value={JSON.stringify(data, null, 2)} readOnly = {true} />}
     </div>
   )
 }
